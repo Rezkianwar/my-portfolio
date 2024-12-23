8d146db8b38bc5a47554b0c155a8d188d7a90f32
Rezkianwar <123230737+Rezkianwar@users.noreply.github.com>
2024-12-23 10:51:11 +0700

    add logo favicon.ico

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,12 @@ import { Testimonials } from "@/components/Testimonials";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+export const metadata = {
+  title: "Portfolio Rezki Anwar",
+  description: "website portfolio",
+  img: "/favicon.ico",
+};
+
 export default function Home() {
   return (
     <>
